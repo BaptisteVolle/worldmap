9586c78a9874fc4aae1ac8226682c043a5fd8f3a
Baptiste Volle <baptistevolle09@gmail.com>
2025-03-24 07:58:17 -0400

    works

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -76,7 +76,7 @@ export class MapComponent implements OnInit, OnDestroy {
       worldCopyJump: false,
       // Set bounds instead
       maxBounds: [
-        [-85, -180], // Southwest corner
+        [-85, -180],
         [85, 180], // Northeast corner
       ],
       maxBoundsViscosity: 1.0, // Make bounds completely solid
@@ -84,21 +84,193 @@ export class MapComponent implements OnInit, OnDestroy {
 
     this.wonderMarkers = this.L.layerGroup();
 
-    // Add base tile layer
-    this.L.tileLayer(
+    const voyagerTiles = this.L.tileLayer(
       'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
       {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>',
         subdomains: 'abcd',
         maxZoom: 20,
-        // Restrict tiles to the bounds
         bounds: [
           [-85, -180],
           [85, 180],
         ],
       }
-    ).addTo(this.map);
+    );
+
+    // Terrain style - shows elevation with colors
+    const terrainTiles = this.L.tileLayer(
+      'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+      {
+        attribution:
+          'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a>',
+        maxZoom: 17,
+        bounds: [
+          [-85, -180],
+          [85, 180],
+        ],
+      }
+    );
+
+    // Dark style - for a different look
+    const darkTiles = this.L.tileLayer(
+      'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+      {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 20,
+        bounds: [
+          [-85, -180],
+          [85, 180],
+        ],
+      }
+    );
+
+    // Add these to your map initialization where you define the other tile layers
+
+    // Positron (Light) - a clean, light-colored base map
+    const positronTiles = this.L.tileLayer(
+      'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+      {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 20,
+        bounds: [
+          [-85, -180],
+          [85, 180],
+        ],
+      }
+    );
+
+    // Satellite imagery
+    const satelliteTiles = this.L.tileLayer(
+      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+      {
+        attribution:
+          'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+        maxZoom: 18,
+        bounds: [
+          [-85, -180],
+          [85, 180],
+        ],
+      }
+    );
+
+    // Toner - Black and white style
+    const tonerTiles = this.L.tileLayer(
+      'https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}.png',
+      {
+        attribution:
+          'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        maxZoom: 20,
+        bounds: [
+          [-85, -180],
+          [85, 180],
+        ],
+      }
+    );
+
+    // Comic style - a fun, colorful style
+    const comicTiles = this.L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 19,
+        className: 'comic-style-map', // We'll add a CSS filter to give it a comic look
+        bounds: [
+          [-85, -180],
+          [85, 180],
+        ],
+      }
+    );
+
+    // Add the terrain style by default - it's more colorful
+    terrainTiles.addTo(this.map);
+
+    // Create a layer control and add it to the map
+    const baseLayers = {
+      Terrain: terrainTiles,
+      Standard: voyagerTiles,
+      Dark: darkTiles,
+      Satellite: satelliteTiles,
+      'Black & White': tonerTiles,
+      Comic: comicTiles,
+      Light: positronTiles,
+    };
+
+    const styleControl = this.L.control({ position: 'topright' });
+    styleControl.onAdd = () => {
+      const div = this.L.DomUtil.create('div', 'map-style-control leaflet-bar');
+      div.innerHTML = `
+    <div class="map-style-title">Map Style</div>
+    <a href="#" class="style-option active" data-style="terrain">Terrain</a>
+    <a href="#" class="style-option" data-style="standard">Standard</a>
+    <a href="#" class="style-option" data-style="dark">Dark</a>
+    <a href="#" class="style-option" data-style="satellite">Satellite</a>
+    <a href="#" class="style-option" data-style="toner">B&W</a>
+    <a href="#" class="style-option" data-style="comic">Comic</a>
+  `;
+
+      this.L.DomEvent.disableClickPropagation(div);
+
+      // Add click handlers
+      setTimeout(() => {
+        const options = div.querySelectorAll('.style-option');
+        options.forEach((option: HTMLElement) => {
+          option.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            // Update active clas
+            options.forEach((o: HTMLElement) => o.classList.remove('active'));
+            option.classList.add('active');
+
+            [
+              terrainTiles,
+              voyagerTiles,
+              darkTiles,
+              satelliteTiles,
+              tonerTiles,
+              comicTiles,
+              positronTiles,
+            ].forEach((layer) => {
+              if (this.map.hasLayer(layer)) {
+                this.map.removeLayer(layer);
+              }
+            });
+
+            const style = option.getAttribute('data-style');
+            switch (style) {
+              case 'terrain':
+                terrainTiles.addTo(this.map);
+                break;
+              case 'standard':
+                voyagerTiles.addTo(this.map);
+                break;
+              case 'dark':
+                darkTiles.addTo(this.map);
+                break;
+              case 'satellite':
+                satelliteTiles.addTo(this.map);
+                break;
+              case 'toner':
+                tonerTiles.addTo(this.map);
+                break;
+              case 'comic':
+                comicTiles.addTo(this.map);
+                break;
+              default:
+                terrainTiles.addTo(this.map);
+            }
+          });
+        });
+      }, 0);
+
+      return div;
+    };
+    styleControl.addTo(this.map);
 
     // Add zoom controls
     this.L.control
